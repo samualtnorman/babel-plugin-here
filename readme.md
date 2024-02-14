4bc0b6037db27b094c6267ab24bef47da01c8e21
Samual Norman <me@samual.uk>
2024-02-14 16:57:13 +0000

    modernise repo

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Babel Plugin Here (`babel-plugin-here`)
-> Replace instances of `HERE` keyword with code position strings.
+> Replace instances of `HERE` identifier with code position strings.
 
 ## Why?
 This plugin is useful when you don't have sourcemaps and you're looking at errors in the console and having trouble knowing where they've come from. You can setup this plugin, and use `HERE` in error message like ``throw new Error(`${HERE} Some useful message`)``.
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import type { PluginObj } from "@babel/core"
 import * as t from "@babel/types"
+import { assert, ensure } from "@samual/lib/assert"
 import * as Path from "path"
 
-export const here = () => /** @type {import("@babel/core").PluginObj} */ ({
+export const babelPluginHere = (): PluginObj => ({
 	name: "babel-plugin-here",
 	visitor: {
 		Program(path) {
@@ -14,28 +16,26 @@ export const here = () => /** @type {import("@babel/core").PluginObj} */ ({
 			)
 
 			path.scope.crawl()
+			const filePath = Path.relative(".", ensure(this.file.opts.filename, HERE))
 
-			const filePath = Path.relative(".", this.file.opts.filename)
-
-			for (const referencePath of path.scope.getBinding("HERE").referencePaths) {
+			for (const referencePath of ensure(path.scope.getBinding("HERE"), HERE).referencePaths) {
+				assert(referencePath.node.loc, HERE)
 				const line = referencePath.node.loc.start.line
 				const column = referencePath.node.loc.start.column + 1
 
 				if (referencePath.parent.type != "TemplateLiteral") {
 					referencePath.replaceWith(t.stringLiteral(`${filePath}:${line}:${column}`))
-
 					continue
 				}
 
 				const { parent, node } = referencePath
-				const index = parent.expressions.indexOf(/** @type {any} */(node))
-				const quasi = parent.quasis[index].value.raw
-				const nextQuasi = parent.quasis[index + 1].value.raw
-
+				const index = parent.expressions.indexOf(node as any)
+				const quasi = ensure(parent.quasis[index], HERE)
+				const nextQuasi = ensure(parent.quasis[index + 1], HERE)
 				parent.expressions.splice(index, 1)
-				delete parent.quasis[index].value.cooked
-				parent.quasis[index].value.raw = `${quasi}${filePath}:${line}:${column}${nextQuasi}`
-				parent.quasis[index].tail = parent.quasis[index + 1].tail
+				delete quasi.value.cooked
+				quasi.value.raw = `${quasi.value.raw}${filePath}:${line}:${column}${nextQuasi.value.raw}`
+				quasi.tail = nextQuasi.tail
 				parent.quasis.splice(index + 1, 1)
 			}
 
@@ -44,4 +44,5 @@ export const here = () => /** @type {import("@babel/core").PluginObj} */ ({
 	}
 })
 
-export default here
+export { babelPluginHere as default, babelPluginHere as here }
+
