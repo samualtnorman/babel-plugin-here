e91d6a98363f718752ad81ee046dae845fabdb34
Samual Norman <me@samual.uk>
2024-02-15 11:04:41 +0000

    omit `"engines"` property in emitted `package.json`

--- scripts/emit-package-json.js
+++ scripts/emit-package-json.js
@@ -6,6 +6,7 @@ const /** @type {Partial<typeof packageJson_>} */ packageJson = packageJson_
 
 delete packageJson.private
 delete packageJson.devDependencies
+delete packageJson.engines
 
 makeDirectorySync("dist", { recursive: true })
 writeFileSync("dist/package.json", JSON.stringify(packageJson))
