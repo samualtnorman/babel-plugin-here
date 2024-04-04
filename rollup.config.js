3e07890b5c2265862f07c92b5b8b9021e9084faa
Samual Norman <me@samual.uk>
2024-04-04 11:29:48 +0100

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,4 @@
 #!node_modules/.bin/rollup --config
-import babelPresetTypescript from "@babel/preset-typescript"
-import { babel } from "@rollup/plugin-babel"
-import { babelPluginHere } from "babel-plugin-here"
-import prettier from "rollup-plugin-prettier"
+import { rollupConfig } from "@samual/rollup-config"
 
-/** @type {import("rollup").RollupOptions} */ export default {
-	external: () => true,
-	input: "src/index.ts",
-	output: { dir: "dist", sourcemapPathTransform: relativeSourcePath => relativeSourcePath.slice(2) },
-	plugins: [
-		babel({
-			babelHelpers: "bundled",
-			extensions: [ ".ts" ],
-			presets: [ babelPresetTypescript ],
-			plugins: [ babelPluginHere() ]
-		}),
-		prettier({
-			parser: "espree",
-			useTabs: true,
-			tabWidth: 4,
-			arrowParens: "avoid",
-			experimentalTernaries: true,
-			printWidth: 120,
-			semi: false,
-			trailingComma: "none"
-		})
-	],
-	strictDeprecations: true,
-	treeshake: { moduleSideEffects: false }
-}
+export default rollupConfig()
