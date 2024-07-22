da830d62d0124644227bd7d6f506c23cd4ff981d
Samual Norman <me@samual.uk>
2024-07-22 15:18:08 +0100

    update

--- scripts/emit-package-json.js
+++ scripts/emit-package-json.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 import { mkdirSync as makeDirectorySync, writeFileSync } from "fs"
-import packageJson_ from "../package.json" assert { type: "json" }
+import packageJson_ from "../package.json" with { type: "json" }
 
 const /** @type {Partial<typeof packageJson_>} */ packageJson = packageJson_
 
 delete packageJson.private
 delete packageJson.devDependencies
 delete packageJson.engines
-
 makeDirectorySync("dist", { recursive: true })
 writeFileSync("dist/package.json", JSON.stringify(packageJson, undefined, "\t"))
 process.exit()
