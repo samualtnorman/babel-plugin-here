d254d5b55dd4376072299c2d2291fe1a2eb47279
Samual Norman <me@samual.uk>
2025-10-17 11:30:38 +0100

    switch to rolldown

---
+++ rolldown.config.js
@@ -0,0 +1,4 @@
+#!node_modules/.bin/rolldown --config
+import { rolldownConfig } from "@samual/rolldown-config"
+
+export default rolldownConfig()
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 rm -rf dist
-./rollup.config.js
+./rolldown.config.js
 scripts/emit-types.sh
 scripts/emit-package-json.js
 cp readme.md license src/env.d.ts dist
