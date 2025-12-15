86f4494801f376244c2566d5748f23be5368f1e1
Samual Norman <me@samual.uk>
2025-12-15 16:13:43 +0000

    remove `@samual/lib` by replacing `@samual/lib/assert` with `@sn/assert`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type { PluginObj } from "@babel/core"
 import * as t from "@babel/types"
-import { assert, ensure } from "@samual/lib/assert"
+import { assert, expect } from "@sn/assert"
 import * as Path from "path"
 
 export const babelPluginHere = (): PluginObj => ({
@@ -24,7 +24,7 @@ export const babelPluginHere = (): PluginObj => ({
 				questionMarkIndex > 0 ? this.file.opts.filename.slice(0, questionMarkIndex) : this.file.opts.filename
 			)
 
-			for (const referencePath of ensure(path.scope.getBinding("HERE"), HERE).referencePaths) {
+			for (const referencePath of expect(path.scope.getBinding("HERE"), HERE).referencePaths) {
 				assert(referencePath.node.loc, HERE)
 				const line = referencePath.node.loc.start.line
 				const column = referencePath.node.loc.start.column + 1
@@ -36,8 +36,8 @@ export const babelPluginHere = (): PluginObj => ({
 
 				const { parent, node } = referencePath
 				const index = parent.expressions.indexOf(node as any)
-				const quasi = ensure(parent.quasis[index], HERE)
-				const nextQuasi = ensure(parent.quasis[index + 1], HERE)
+				const quasi = expect(parent.quasis[index], HERE)
+				const nextQuasi = expect(parent.quasis[index + 1], HERE)
 				parent.expressions.splice(index, 1)
 				delete quasi.value.cooked
 				quasi.value.raw = `${quasi.value.raw}${filePath}:${line}:${column}${nextQuasi.value.raw}`
